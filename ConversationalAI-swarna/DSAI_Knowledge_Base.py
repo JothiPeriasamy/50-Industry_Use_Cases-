25345b7f48c13d3e4e8745c17133eb5727bea338
DeepsphereAI <DeepSphereAIInfo@gmail.com>
2022-05-10 20:36:28 +0530

    commit

--- ConversationalAI-swarna/DSAI_Knowledge_Base.py
+++ ConversationalAI-swarna/DSAI_Knowledge_Base.py
@@ -4,84 +4,84 @@ import csv
 import spotlight
 
 # define namespaces
-ex = Namespace("http://example.org/")
+vAR_ex = Namespace("http://example.org/")
 exdata = Namespace("http://example.org/data#")
 
-g = Graph()
+vAR_g = Graph()
 
 # create knowledge base
-g.add( (ex.University, RDF.type, RDFS.Class) )
-g.add( (ex.University, RDFS.subClassOf, FOAF.Organization) )
-g.add( (ex.University, RDFS.label, Literal("University", lang="en")) )
-g.add( (ex.University, RDFS.comment, Literal("Organization at which the students go to")) )
+vAR_g.add( (vAR_ex.University, RDF.type, RDFS.Class) )
+vAR_g.add( (vAR_ex.University, RDFS.subClassOf, FOAF.Organization) )
+vAR_g.add( (vAR_ex.University, RDFS.label, Literal("University", lang="en")) )
+vAR_g.add( (vAR_ex.University, RDFS.comment, Literal("Organization at which the students go to")) )
 
-g.add( (ex.Course, RDF.type, RDFS.Class) )
-g.add( (ex.Course, RDFS.label, Literal("Course", lang="en")) )
-g.add( (ex.Course, RDFS.comment, Literal("Course is offered at a university_data towards the granting of an approved degree")) )
-g.add( (ex.Course, FOAF.name, XSD.string) )
-g.add( (ex.Course, ex.hasSubject, XSD.string) )
-g.add( (ex.Course, ex.hasNumber, XSD.integer) )
-g.add( (ex.Course, ex.hasDescription, XSD.string) )
-g.add( (ex.Course, RDFS.seeAlso, XSD.anyURI) )
-g.add( (ex.Course, ex.hasTopic, ex.Topic) )
+vAR_g.add( (vAR_ex.Course, RDF.type, RDFS.Class) )
+vAR_g.add( (vAR_ex.Course, RDFS.label, Literal("Course", lang="en")) )
+vAR_g.add( (vAR_ex.Course, RDFS.comment, Literal("Course is offered at a university_data towards the granting of an approved degree")) )
+vAR_g.add( (vAR_ex.Course, FOAF.name, XSD.string) )
+vAR_g.add( (vAR_ex.Course, vAR_ex.hasSubject, XSD.string) )
+vAR_g.add( (vAR_ex.Course, vAR_ex.hasNumber, XSD.integer) )
+vAR_g.add( (vAR_ex.Course, vAR_ex.hasDescription, XSD.string) )
+vAR_g.add( (vAR_ex.Course, RDFS.seeAlso, XSD.anyURI) )
+vAR_g.add( (vAR_ex.Course, vAR_ex.hasTopic, vAR_ex.Topic) )
 
-g.add( (ex.Topic, RDF.type, RDFS.Class) )
-g.add( (ex.Topic, RDFS.label, Literal("Topic", lang="en")) )
-g.add( (ex.Topic, RDFS.comment, Literal("Topic is part of a course material")) )
-g.add( (ex.Topic, FOAF.name, XSD.string) )
-g.add( (ex.Topic, RDFS.seeAlso, XSD.anyURI) )
+vAR_g.add( (vAR_ex.Topic, RDF.type, RDFS.Class) )
+vAR_g.add( (vAR_ex.Topic, RDFS.label, Literal("Topic", lang="en")) )
+vAR_g.add( (vAR_ex.Topic, RDFS.comment, Literal("Topic is part of a course material")) )
+vAR_g.add( (vAR_ex.Topic, FOAF.name, XSD.string) )
+vAR_g.add( (vAR_ex.Topic, RDFS.seeAlso, XSD.anyURI) )
 
-g.add( (ex.Student, RDF.type, RDFS.Class) )
-g.add( (ex.Student, RDFS.subClassOf, FOAF.Person) )
-g.add( (ex.Student, RDFS.label, Literal("Student", lang="en")) )
-g.add( (ex.Student, RDFS.comment, Literal("Person who studies at a university_data")) )
-g.add( (ex.Student, FOAF.name, XSD.string) )
-g.add( (ex.Student, ex.hasID, XSD.integer) )
-g.add( (ex.Student, FOAF.mbox, XSD.string) )
-g.add( (ex.Student, ex.hasCompleted, ex.Course) )
+vAR_g.add( (vAR_ex.Student, RDF.type, RDFS.Class) )
+vAR_g.add( (vAR_ex.Student, RDFS.subClassOf, FOAF.Person) )
+vAR_g.add( (vAR_ex.Student, RDFS.label, Literal("Student", lang="en")) )
+vAR_g.add( (vAR_ex.Student, RDFS.comment, Literal("Person who studies at a university_data")) )
+vAR_g.add( (vAR_ex.Student, FOAF.name, XSD.string) )
+vAR_g.add( (vAR_ex.Student, vAR_ex.hasID, XSD.integer) )
+vAR_g.add( (vAR_ex.Student, FOAF.mbox, XSD.string) )
+vAR_g.add( (vAR_ex.Student, vAR_ex.hasCompleted, vAR_ex.Course) )
 
-g.add( (ex.hasSubject, RDF.type, RDF.Property) )
-g.add( (ex.hasSubject, RDFS.label, Literal("hasSubject", lang="en")) )
-g.add( (ex.hasSubject, RDFS.comment, Literal("Course has a subject")) )
-g.add( (ex.hasSubject, RDFS.domain, ex.Course) )
-g.add( (ex.hasSubject, RDFS.range, XSD.string) )
+vAR_g.add( (vAR_ex.hasSubject, RDF.type, RDF.Property) )
+vAR_g.add( (vAR_ex.hasSubject, RDFS.label, Literal("hasSubject", lang="en")) )
+vAR_g.add( (vAR_ex.hasSubject, RDFS.comment, Literal("Course has a subject")) )
+vAR_g.add( (vAR_ex.hasSubject, RDFS.domain, vAR_ex.Course) )
+vAR_g.add( (vAR_ex.hasSubject, RDFS.range, XSD.string) )
 
-g.add( (ex.hasNumber, RDF.type, RDF.Property) )
-g.add( (ex.hasNumber, RDFS.label, Literal("hasNumber", lang="en")) )
-g.add( (ex.hasNumber, RDFS.comment, Literal("Course has a number")) )
-g.add( (ex.hasNumber, RDFS.domain, ex.Course) )
-g.add( (ex.hasNumber, RDFS.range, XSD.integer) )
+vAR_g.add( (vAR_ex.hasNumber, RDF.type, RDF.Property) )
+vAR_g.add( (vAR_ex.hasNumber, RDFS.label, Literal("hasNumber", lang="en")) )
+vAR_g.add( (vAR_ex.hasNumber, RDFS.comment, Literal("Course has a number")) )
+vAR_g.add( (vAR_ex.hasNumber, RDFS.domain, vAR_ex.Course) )
+vAR_g.add( (vAR_ex.hasNumber, RDFS.range, XSD.integer) )
 
-g.add( (ex.hasDescription, RDF.type, RDF.Property) )
-g.add( (ex.hasDescription, RDFS.label, Literal("hasDescription", lang="en")) )
-g.add( (ex.hasDescription, RDFS.comment, Literal("Course has a description")) )
-g.add( (ex.hasDescription, RDFS.domain, ex.Course) )
-g.add( (ex.hasDescription, RDFS.range, XSD.string) )
+vAR_g.add( (vAR_ex.hasDescription, RDF.type, RDF.Property) )
+vAR_g.add( (vAR_ex.hasDescription, RDFS.label, Literal("hasDescription", lang="en")) )
+vAR_g.add( (vAR_ex.hasDescription, RDFS.comment, Literal("Course has a description")) )
+vAR_g.add( (vAR_ex.hasDescription, RDFS.domain, vAR_ex.Course) )
+vAR_g.add( (vAR_ex.hasDescription, RDFS.range, XSD.string) )
 
-g.add( (ex.hasID, RDF.type, RDF.Property) )
-g.add( (ex.hasID, RDFS.label, Literal("hasID", lang="en")) )
-g.add( (ex.hasID, RDFS.comment, Literal("Student has an ID number")) )
-g.add( (ex.hasID, RDFS.domain, ex.Student) )
-g.add( (ex.hasID, RDFS.range, XSD.integer) )
+vAR_g.add( (vAR_ex.hasID, RDF.type, RDF.Property) )
+vAR_g.add( (vAR_ex.hasID, RDFS.label, Literal("hasID", lang="en")) )
+vAR_g.add( (vAR_ex.hasID, RDFS.comment, Literal("Student has an ID number")) )
+vAR_g.add( (vAR_ex.hasID, RDFS.domain, vAR_ex.Student) )
+vAR_g.add( (vAR_ex.hasID, RDFS.range, XSD.integer) )
 
-g.add( (ex.hasTopic, RDF.type, RDF.Property) )
-g.add( (ex.hasTopic, RDFS.label, Literal("hasTopic", lang="en")) )
-g.add( (ex.hasTopic, RDFS.comment, Literal("Course has a topic")) )
-g.add( (ex.hasTopic, RDFS.domain, ex.Course) )
-g.add( (ex.hasTopic, RDFS.range, ex.Topic) )
+vAR_g.add( (vAR_ex.hasTopic, RDF.type, RDF.Property) )
+vAR_g.add( (vAR_ex.hasTopic, RDFS.label, Literal("hasTopic", lang="en")) )
+vAR_g.add( (vAR_ex.hasTopic, RDFS.comment, Literal("Course has a topic")) )
+vAR_g.add( (vAR_ex.hasTopic, RDFS.domain, vAR_ex.Course) )
+vAR_g.add( (vAR_ex.hasTopic, RDFS.range, vAR_ex.Topic) )
 
-g.add( (ex.hasCompleted, RDF.type, RDF.Property) )
-g.add( (ex.hasCompleted, RDFS.label, Literal("hasCompleted", lang="en")) )
-g.add( (ex.hasCompleted, RDFS.comment, Literal("Student has completed a course")) )
-g.add( (ex.hasCompleted, RDFS.domain, ex.Student) )
-g.add( (ex.hasCompleted, RDFS.range, ex.Course) )
+vAR_g.add( (vAR_ex.hasCompleted, RDF.type, RDF.Property) )
+vAR_g.add( (vAR_ex.hasCompleted, RDFS.label, Literal("hasCompleted", lang="en")) )
+vAR_g.add( (vAR_ex.hasCompleted, RDFS.comment, Literal("Student has completed a course")) )
+vAR_g.add( (vAR_ex.hasCompleted, RDFS.domain, vAR_ex.Student) )
+vAR_g.add( (vAR_ex.hasCompleted, RDFS.range, vAR_ex.Course) )
 
-g.add( (ex.hasGrade, RDF.type, RDF.Property) )
-g.add( (ex.hasGrade, RDFS.subPropertyOf, ex.hasCompleted) )
-g.add( (ex.hasGrade, RDFS.label, Literal("hasGrade", lang="en")) )
-g.add( (ex.hasGrade, RDFS.comment, Literal("Student has a grade for a completed course")) )
-g.add( (ex.hasGrade, RDFS.domain, ex.hasCompleted) )
-g.add( (ex.hasGrade, RDFS.range, XSD.string ) )
+vAR_g.add( (vAR_ex.hasGrade, RDF.type, RDF.Property) )
+vAR_g.add( (vAR_ex.hasGrade, RDFS.subPropertyOf, vAR_ex.hasCompleted) )
+vAR_g.add( (vAR_ex.hasGrade, RDFS.label, Literal("hasGrade", lang="en")) )
+vAR_g.add( (vAR_ex.hasGrade, RDFS.comment, Literal("Student has a grade for a completed course")) )
+vAR_g.add( (vAR_ex.hasGrade, RDFS.domain, vAR_ex.hasCompleted) )
+vAR_g.add( (vAR_ex.hasGrade, RDFS.range, XSD.string ) )
 
 # processing university_data data into RDF triples
 with open("D:\\ConvAI\\DSAI_University_Data_1") as data:
@@ -90,9 +90,9 @@ with open("D:\\ConvAI\\DSAI_University_Data_1") as data:
         university = URIRef(exdata + row[0].replace(" ", "_")) # define university URI using first column
         link = URIRef(row[1]) # define link URI to university's entry in dbpedia using second column
 
-        g.add( (university, RDF.type, ex.University) )
-        g.add( (university, FOAF.name, Literal(row[0])) )
-        g.add( (university, RDFS.seeAlso, link) )
+        vAR_g.add( (university, RDF.type, vAR_ex.University) )
+        vAR_g.add( (university, FOAF.name, Literal(row[0])) )
+        vAR_g.add( (university, RDFS.seeAlso, link) )
 
 # processing course data into RDF triples
 with open("D:\\ConvAI\\DSAI_Course_Data_1") as data:
@@ -101,11 +101,11 @@ with open("D:\\ConvAI\\DSAI_Course_Data_1") as data:
         course = URIRef(exdata + row[0].replace(" ", "_")) # define course URI using first column
         link = URIRef(row[3]) # define link URI to online source of course using fourth column
 
-        g.add( (course, RDF.type, ex.Course) )
-        g.add( (course, FOAF.name, Literal(row[0])) )
-        g.add( (course, ex.hasSubject, Literal(row[1])) )
-        g.add( (course, ex.hasNumber, Literal(row[2])) )
-        g.add( (course, RDFS.seeAlso, link) )
+        vAR_g.add( (course, RDF.type, vAR_ex.Course) )
+        vAR_g.add( (course, FOAF.name, Literal(row[0])) )
+        vAR_g.add( (course, vAR_ex.hasSubject, Literal(row[1])) )
+        vAR_g.add( (course, vAR_ex.hasNumber, Literal(row[2])) )
+        vAR_g.add( (course, RDFS.seeAlso, link) )
 
         try:
             # use dbpedia spotlight to find topics
@@ -118,14 +118,14 @@ with open("D:\\ConvAI\\DSAI_Course_Data_1") as data:
                 topicLink = URIRef(topicRow['URI']) # define link URI to dbpedia source of the topic
 
                 # only add topic to graph if not already in graph
-                for s, p, o in g:
-                    if not (topic, RDF.type, ex.Topic) in g:
-                        g.add( (topic, RDF.type, ex.Topic) )
-                        g.add( (topic, FOAF.name, Literal(topicRow['surfaceForm'])) )
-                        g.add( (topic, RDFS.seeAlso, topicLink) )
+                for s, p, o in vAR_g:
+                    if not (topic, RDF.type, vAR_ex.Topic) in vAR_g:
+                        vAR_g.add( (topic, RDF.type, vAR_ex.Topic) )
+                        vAR_g.add( (topic, FOAF.name, Literal(topicRow['surfaceForm'])) )
+                        vAR_g.add( (topic, RDFS.seeAlso, topicLink) )
 
                 # add topic to this course
-                g.add( (course, ex.hasTopic, topic))
+                vAR_g.add( (course, vAR_ex.hasTopic, topic))
         except:
             print()
 
@@ -138,23 +138,23 @@ with open("D:\\ConvAI\\DSAI_Student_Data_1") as data:
         course = URIRef(exdata + row[3].replace(" ", "_")) # define course URI using fourth column
 
         # only add student to graph if not already in graph
-        for s, p, o in g:
-            if not (student, RDF.type, ex.Student) in g:
-                g.add( (student, RDF.type, ex.Student) )
-                g.add( (student, FOAF.name, Literal(row[0])) )
-                g.add( (student, ex.hasID, Literal(row[1])) )
-                g.add( (student, FOAF.mbox, Literal(row[2])) )
+        for s, p, o in vAR_g:
+            if not (student, RDF.type, vAR_ex.Student) in vAR_g:
+                vAR_g.add( (student, RDF.type, vAR_ex.Student) )
+                vAR_g.add( (student, FOAF.name, Literal(row[0])) )
+                vAR_g.add( (student, vAR_ex.hasID, Literal(row[1])) )
+                vAR_g.add( (student, FOAF.mbox, Literal(row[2])) )
 
                 if not (row[3] == ''):
-                    g.add( (student, ex.hasCompleted, course) )
+                    vAR_g.add( (student, vAR_ex.hasCompleted, course) )
                 if not (row[4] == ''):
-                    g.add( (ex.hasCompleted, ex.hasGrade, Literal(row[4])) )
+                    vAR_g.add( (vAR_ex.hasCompleted, vAR_ex.hasGrade, Literal(row[4])) )
             else:
                 if not (row[3] == ''):
-                    g.add( (student, ex.hasCompleted, course) )
+                    vAR_g.add( (student, vAR_ex.hasCompleted, course) )
                 if not (row[4] == ''):
-                    g.add( (ex.hasCompleted, ex.hasGrade, Literal(row[4])) )
+                    vAR_g.add( (vAR_ex.hasCompleted, vAR_ex.hasGrade, Literal(row[4])) )
 
 # print graph in N-Triples format to knowledge_base.nt file
 # run this only once to populate the .nt file
-print(g.serialize("D:\\DSAI_Conversational_AI\\Knowledge_Graph\\Utility\\DSAI_Knowledge_Base.nt", format="nt"))
+print(vAR_g.serialize("D:\\DSAI_Conversational_AI\\Knowledge_Graph\\Utility\\DSAI_Knowledge_Base.nt", format="nt"))
